cdabf165c741cefbf1973d4001734540261e6ea6
Agustín Cassani <agustinscassani@gmail.com>
2026-09-03 14:46:57 -0300

    Apuntar las descargas a los APK recién subidos
    Los enlaces de Drive de la app de beneficiario y de caja apuntaban a builds anteriores.

--- lib/mobile-apps.ts
+++ lib/mobile-apps.ts
@@ -1,5 +1,5 @@
 // Google Drive APK download links for the Puntos Club mobile apps.
 export const PUNTOS_CLUB_APK_URL =
-  "https://drive.google.com/file/d/1MthKz0KKbsyoiMW8wUwMyzhcNQYFCfn3/view?usp=drive_link";
+  "https://drive.google.com/file/d/1XmVvLUcu9Ynrk3Fzhhg1vYRKFI_Bbp4q/view?usp=drive_link";
 export const PUNTOS_CLUB_CAJA_APK_URL =
-  "https://drive.google.com/file/d/1BulBkyKT3A7X964ULGybsS7VO-x0FliQ/view?usp=drive_link";
+  "https://drive.google.com/file/d/1fnZiIedU-7AgFf7n4PbhMdtJZ9wXtHO9/view?usp=drive_link";
